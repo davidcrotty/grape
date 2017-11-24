799921dc77806d8c8c37cd9b1df288b4218976b1
David Crotty <david.crotty.work@gmail.com>
2017-11-24 22:46:39 +0000

    returns simple JSON

--- api/BikeController.rb
+++ api/BikeController.rb
@@ -1,10 +1,12 @@
 module Controller
   class BikeController < Grape::API
     version 'v1'
+    format :json
+
     resource :bike do
       desc 'Manages bikes'
       get do
-        'bike'
+        { :id => '1', :frame => 'shimano' }
       end
     end
   end
