d6932668acf17d8307c1b1eb9c805451ff57b44f
David Crotty <david.crotty.work@gmail.com>
2017-11-24 23:05:55 +0000

    Adds required parameters to request

--- api/BikeController.rb
+++ api/BikeController.rb
@@ -2,11 +2,20 @@ module Controller
   class BikeController < Grape::API
     version 'v1'
     format :json
-
     resource :bike do
       desc 'Manages bikes'
+
       get '/:bikeid' do
         { id: params[:bikeid], frame: 'shimano' }
+      end
+
+      params do
+        requires :id, type: Integer
+      end
+
+      post do
+        status 204
+        body false
       end
     end
   end
